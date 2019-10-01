4296ca3291fb68bd2b51541bbc94fafeaea65e1d
Luke Browne <S00187306@mail.itsligo.ie>
2019-10-01 16:32:45 +0100

    Add project files.

---
+++ BankAccounts/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
